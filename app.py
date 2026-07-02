960f466abcf931af71514d28240a2ef198757c25
Vamsi <vk920926123@gmail.com>
2026-07-02 15:49:08 +0530

    Fixed Render port

--- app.py
+++ app.py
@@ -381,5 +381,8 @@ def logout():
 
 # ---------------- RUN APPLICATION ---------------- #
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
